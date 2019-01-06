5db82ffdf467804bc3a86b96e48432acaeeaf7be
Ricardo Casares <ricardocasares@gmail.com>
2019-01-06 04:29:52 +0100

    feat(cors): enable cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const polka = require("polka");
+const cors = require("cors");
 const graphql = require("express-graphql");
 const schema = require("./schema");
 
 polka()
+  .use(cors())
   .use(
     "/",
     graphql({
